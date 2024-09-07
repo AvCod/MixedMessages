a0ec10498d893ab5492a886cda46f2a643bb0f54
Akshay Bathini <akshaybathini@gmail.com>
2024-09-07 18:20:41 +0530

    Add Randomnum Function and value holder object

---
+++ script.js
@@ -0,0 +1,39 @@
+const randIndex = num => {
+    return Math.floor(Math.random() * num);
+};
+
+const inspirationalQuote = {
+     Avatar: [
+        "Po",
+        "Master Shifu",
+        "Tigress",
+        "Monkey",
+        "Crane",
+        "Viper",
+        "Mantis",
+        "Tai Lung",
+        "Lord Shen"
+      ],
+     Abilities: [
+        "Dragon Warrior",
+        "Exceptional Agility",
+        "Master of Tiger Style",
+        "Acrobatics",
+        "Flight",
+        "Flexibility",
+        "Speed",
+        "Paw of the Furious Lotus",
+        "Fireworks Weaponry"
+      ],
+     Quotes: [
+        "There is no secret ingredient. It's just you.",
+        "The past is history, the future is a mystery, but today is a gift. That’s why it’s called the present.",
+        "One often meets his destiny on the road he takes to avoid it.",
+        "To make something special, you just have to believe it's special.",
+        "You are the Dragon Warrior. You have to believe it.",
+        "The more you take, the more you leave behind. What am I?",
+        "If you only do what you can do, you will never be more than you are now.",
+        "It's not about how hard you hit. It's about how hard you can get hit and keep moving forward.",
+        "You should focus on the present. You should focus on the present."
+      ]
+};
